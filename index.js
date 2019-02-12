1c9edc790b31d09b6b83f3c0371e2b8a654d9a4d
Catherine Batsoula <cbatsoula@gmail.com>
2019-02-12 19:55:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// write your code below!
+
+function happyHolidays(string) {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(string) {
+  if (name)
+  return "Happy holidays, ${name}!";
+
+}
+
+function happyHolidayTo(string) {
+  return "Happy ${holiday}, ${name}!";
+}
+
+function holidayCountdown(string) {
+  
+  return "It's ${days} days until ${holiday}!";
+}
